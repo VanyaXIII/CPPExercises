84e4755effe1ab6b0be3fab95094cdefa01faff9
VanyaXIII <guryanoviv@gmail.com>
2022-03-01 20:51:51 +0300

    lesson 15

--- lesson15/src/mainDijkstra.cpp
+++ lesson15/src/mainDijkstra.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <iostream>
 #include <stdexcept>
+#include <queue>
 
 int debugPoint(int line) {
     if (line < 0)
@@ -18,8 +19,7 @@ struct Edge {
     int u, v; // номера вершин которые это ребро соединяет
     int w; // длина ребра (т.е. насколько длинный путь предстоит преодолеть переходя по этому ребру между вершинами)
 
-    Edge(int u, int v, int w) : u(u), v(v), w(w)
-    {}
+    Edge(int u, int v, int w) : u(u), v(v), w(w) {}
 };
 
 void run() {
@@ -29,50 +29,65 @@ void run() {
     // Превышено ограничение времени на тесте 31
     //
     // То все замечательно и вы молодец.
-
-    int nvertices, medges;
-    std::cin >> nvertices;
+    std::cin.tie(nullptr);
+    std::ios ::sync_with_stdio(false);
+    int n, medges;
+    std::cin >> n;
     std::cin >> medges;
-
-    std::vector<std::vector<Edge>> edges_by_vertex(nvertices);
+    std::vector<std::vector<Edge>> edges_by_vertex(n);
     for (int i = 0; i < medges; ++i) {
         int ai, bi, w;
         std::cin >> ai >> bi >> w;
-        rassert(ai >= 1 && ai <= nvertices, 23472894792020);
-        rassert(bi >= 1 && bi <= nvertices, 23472894792021);
+        rassert(ai >= 1 && ai <= n, 23472894792020);
+        rassert(bi >= 1 && bi <= n, 23472894792021);
 
         ai -= 1;
         bi -= 1;
-        rassert(ai >= 0 && ai < nvertices, 3472897424024);
-        rassert(bi >= 0 && bi < nvertices, 3472897424025);
+        rassert(ai >= 0 && ai < n, 3472897424024);
+        rassert(bi >= 0 && bi < n, 3472897424025);
 
         Edge edgeAB(ai, bi, w);
         edges_by_vertex[ai].push_back(edgeAB);
 
-        edges_by_vertex[bi].push_back(Edge(bi, ai, w)); // а тут - обратное ребро, можно конструировать объект прямо в той же строчке где он и потребовался
+        edges_by_vertex[bi].emplace_back(bi, ai,
+                                         w); // а тут - обратное ребро, можно конструировать объект прямо в той же строчке где он и потребовался
     }
 
-    const int start = 0;
-    const int finish = nvertices - 1;
 
     const int INF = std::numeric_limits<int>::max();
 
-    std::vector<int> distances(nvertices, INF);
-    // TODO ...
+    std::vector<int> d(n, INF);
+    d[0] = 0;
+    std::vector<int> p(n, -1);
+    std::priority_queue < std::pair<int,int> > q;
+    q.push (std::make_pair (0, 0));
+    while (!q.empty()) {
+        int v = q.top().second,  cur_d = -q.top().first;
+        q.pop();
+        if (cur_d > d[v])  continue;
 
-//    while (true) {
-//
-//    }
+        for (auto & edge : edges_by_vertex[v]) {
+            int to = edge.v,
+                    len = edge.w;
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.push (std::make_pair (-d[to], to));
+            }
+        }
+    }
 
-//    if (...) {
-//        ...
-//        for (...) {
-//            std::cout << (path[i] + 1) << " ";
-//        }
-//        std::cout << std::endl;
-//    } else {
-//        std::cout << -1 << std::endl;
-//    }
+    if (d[n-1] == INF) {
+        std::cout << -1;
+        return;
+    }
+    std::vector<int> path;
+    for (int v= n - 1; v!=0; v=p[v])
+	    path.push_back (v);
+    path.push_back (0);
+    reverse (path.begin(), path.end());
+    for (auto & i : path)
+        std:: cout << i + 1 << " ";
 }
 
 int main() {
--- lesson15/src/mainMaze.cpp
+++ lesson15/src/mainMaze.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <iostream>
 #include <stdexcept>
+#include <queue>
 
 #include <libutils/rasserts.h>
 
@@ -9,13 +10,14 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core/types.hpp>
 
+#define ll long long
+
 
 struct Edge {
     int u, v; // номера вершин которые это ребро соединяет
-    int w; // длина ребра (т.е. насколько длинный путь предстоит преодолеть переходя по этому ребру между вершинами)
+    ll w; // длина ребра (т.е. насколько длинный путь предстоит преодолеть переходя по этому ребру между вершинами)
 
-    Edge(int u, int v, int w) : u(u), v(v), w(w)
-    {}
+    Edge(int u, int v, int w) : u(u), v(v), w(w) {}
 };
 
 // Эта биективная функция по координате пикселя (строчка и столбик) + размерам картинки = выдает номер вершины
@@ -30,15 +32,24 @@ int encodeVertex(int row, int column, int nrows, int ncolumns) {
 cv::Point2i decodeVertex(int vertexId, int nrows, int ncolumns) {
 
     // TODO: придумайте как найти номер строки и столбика пикселю по номеру вершины (просто поймите предыдущую функцию и эта функция не будет казаться сложной)
-    int row = -1;
-    int column = -1;
+    int row = vertexId / ncolumns;
+    int column = vertexId % ncolumns;
 
     // сверим что функция симметрично сработала:
     rassert(encodeVertex(row, column, nrows, ncolumns) == vertexId, 34782974923035);
 
     rassert(row < nrows, 34723894720027);
     rassert(column < ncolumns, 3824598237592030);
-    return cv::Point2i(column, row);
+    return {column, row};
+}
+
+ll distance(const std::vector<int>& v1, const std::vector<int>& v2){
+    double res = 0;
+    for (int i = 0; i < v1.size(); ++i)
+    {
+        res += pow(v1[i] - v2[i], 2);
+    }
+    return round(sqrt(res));
 }
 
 void run(int mazeNumber) {
@@ -47,17 +58,38 @@ void run(int mazeNumber) {
     rassert(maze.type() == CV_8UC3, 3447928472389020);
     std::cout << "Maze resolution: " << maze.cols << "x" << maze.rows << std::endl;
 
-    int nvertices = 0; // TODO
+    int n = maze.cols * maze.rows;
 
-    std::vector<std::vector<Edge>> edges_by_vertex(nvertices);
-    for (int j = 0; j < maze.rows; ++j) {
-        for (int i = 0; i < maze.cols; ++i) {
-            cv::Vec3b color = maze.at<cv::Vec3b>(j, i);
+    const int INF = std::numeric_limits<int>::max();
+    std::vector<std::vector<Edge>> edges_by_vertex(n);
+    for (int i = 0; i < maze.rows; ++i) {
+        for (int j = 0; j < maze.cols; ++j) {
+            cv::Vec3b color = maze.at<cv::Vec3b>(i,j);
             unsigned char blue = color[0];
             unsigned char green = color[1];
             unsigned char red = color[2];
 
-            // TODO добавьте соотвтетсвующие этому пикселю ребра
+            auto ai = encodeVertex(i,j,maze.rows,maze.cols);
+            if(i > 0) {
+                cv::Vec3b color2 = maze.at<cv::Vec3b>(i-1,j);
+                edges_by_vertex[ai].emplace_back(ai, encodeVertex(i-1,j, maze.rows, maze.cols),
+                                                 std::max(distance({color2[0], color2[1], color2[2]}, {red, green, blue}), 1ll));
+            }
+            if(i < maze.rows-1) {
+                cv::Vec3b color2 = maze.at<cv::Vec3b>(i+1,j);
+                edges_by_vertex[ai].emplace_back(ai, encodeVertex(i+1,j, maze.rows, maze.cols),
+                                                 std::max(distance({color2[0], color2[1], color2[2]}, {red, green, blue}), 1ll));
+            }
+            if(j > 0) {
+                cv::Vec3b color2 = maze.at<cv::Vec3b>(i,j-1);
+                edges_by_vertex[ai].emplace_back(ai, encodeVertex(i,j-1, maze.rows, maze.cols),
+                                                 std::max(distance({color2[0], color2[1], color2[2]}, {red, green, blue}), 1ll));
+            }
+            if(j < maze.cols-1) {
+                cv::Vec3b color2 = maze.at<cv::Vec3b>(i,j+1);
+                edges_by_vertex[ai].emplace_back(ai, encodeVertex(i,j+1, maze.rows, maze.cols),
+                                                 std::max(distance({color2[0], color2[1], color2[2]}, {red, green, blue}), 1ll));
+            }
         }
     }
 
@@ -68,18 +100,55 @@ void run(int mazeNumber) {
     } else if (mazeNumber == 4) {
         start = encodeVertex(154, 312, maze.rows, maze.cols);
         finish = encodeVertex(477, 312, maze.rows, maze.cols);
-    } else if (mazeNumber == 5) { // Лабиринт в большом разрешении, добровольный (на случай если вы реализовали быструю Дейкстру с приоритетной очередью)
+    } else if (mazeNumber ==
+               5) { // Лабиринт в большом разрешении, добровольный (на случай если вы реализовали быструю Дейкстру с приоритетной очередью)
         start = encodeVertex(1200, 1200, maze.rows, maze.cols);
         finish = encodeVertex(1200, 1200, maze.rows, maze.cols);
     } else {
         rassert(false, 324289347238920081);
     }
 
-    const int INF = std::numeric_limits<int>::max();
-
     cv::Mat window = maze.clone(); // на этой картинке будем визуализировать до куда сейчас дошла прокладка маршрута
 
-    std::vector<int> distances(nvertices, INF);
+    std::vector<ll> d(n, INF);
+    d[start] = 0;
+    std::vector<int> p(n, -1);
+    std::priority_queue < std::pair<ll,int> > q;
+    q.push (std::make_pair (0, start));
+    while (!q.empty()) {
+        int v = q.top().second,  cur_d = -q.top().first;
+        q.pop();
+        if (cur_d > d[v])  continue;
+
+        for (auto & edge : edges_by_vertex[v]) {
+            int to = edge.v,
+                    len = edge.w;
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.push (std::make_pair (-d[to], to));
+            }
+        }
+    }
+
+    if (d[n-1] == INF) {
+        std::cout << -1;
+        return;
+    }
+    std::vector<int> path;
+    for (int v = finish; v!=start; v=p[v])
+        path.push_back (v);
+    path.push_back (0);
+    reverse (path.begin(), path.end());
+    for (auto & pa : path)
+    {
+        cv::Point2i p = decodeVertex(pa, maze.rows, maze.cols);
+            window.at<cv::Vec3b>(p.y, p.x) = cv::Vec3b(0, 0, 255);
+            cv::Mat sh = window.clone();
+            cv::resize(window, sh, cv::Size(500, 500), cv::INTER_LINEAR);
+            cv::imshow("Maze", sh);
+            cv::waitKey(1);
+    }
     // TODO СКОПИРУЙТЕ СЮДА ДЕЙКСТРУ ИЗ ПРЕДЫДУЩЕГО ИСХОДНИКА
 
     // TODO в момент когда вершина становится обработанной - красьте ее на картинке window в зеленый цвет и показывайте картинку:
@@ -96,14 +165,16 @@ void run(int mazeNumber) {
     std::cout << "Finished!" << std::endl;
 
     // Показываем результат пока пользователь не насладиться до конца и не нажмет Escape
+    cv::Mat sh = window.clone();
+    cv::resize(window, sh, cv::Size(500, 500), cv::INTER_LINEAR);
     while (cv::waitKey(10) != 27) {
-        cv::imshow("Maze", window);
+        cv::imshow("Maze", sh);
     }
 }
 
 int main() {
     try {
-        int mazeNumber = 1;
+        int mazeNumber =5;
         run(mazeNumber);
 
         return 0;
